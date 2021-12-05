89a6f5a49f8db1fed420957447144b80dad1f993
deatil <2217957370@qq.com>
2021-12-05 12:26:15 +0800

    更新

--- src/views/admin/attachment/list/index.vue
+++ src/views/admin/attachment/list/index.vue
@@ -1,6 +1,77 @@
 <template>
   <div class="p-4">
     <BasicTable @register="registerTable">
+      <template #form-custom> custom-slot </template>
+      <template #headerTop>
+        <a-alert type="info" show-icon>
+          <template #message>
+            <template v-if="checkedKeys.length > 0">
+              <span>已选中{{ checkedKeys.length }}条记录(可跨页)</span>
+              <a-button type="link" @click="checkedKeys = []" size="small">清空</a-button>
+            </template>
+            <template v-else>
+              <span>未选中任何项目</span>
+            </template>
+          </template>
+        </a-alert>
+      </template>
+
+      <template #toolbar>
+        <a-button type="primary" @click="handleReloadCurrent"> 刷新当前页 </a-button>
+        <a-button type="primary" @click="handleReload"> 刷新并返回第一页 </a-button>
+      </template>
+
+      <template #action="{ record }">
+        <TableAction
+          :actions="[
+            {
+              label: '编辑',
+              onClick: handleEdit.bind(null, record),
+              auth: 'other', // 根据权限控制是否显示: 无权限，不显示
+            },
+            {
+              label: '删除',
+              icon: 'ic:outline-delete-outline',
+              onClick: handleDelete.bind(null, record),
+              auth: 'super', // 根据权限控制是否显示: 有权限，会显示
+            },
+          ]"
+          :dropDownActions="[
+            {
+              label: '启用',
+              popConfirm: {
+                title: '是否启用？',
+                confirm: handleOpen.bind(null, record),
+              },
+              ifShow: (_action) => {
+                return record.status !== 'enable'; // 根据业务控制是否显示: 非enable状态的不显示启用按钮
+              },
+            },
+            {
+              label: '禁用',
+              popConfirm: {
+                title: '是否禁用？',
+                confirm: handleOpen.bind(null, record),
+              },
+              ifShow: () => {
+                return record.status === 'enable'; // 根据业务控制是否显示: enable状态的显示禁用按钮
+              },
+            },
+            {
+              label: '同时控制',
+              popConfirm: {
+                title: '是否动态显示？',
+                confirm: handleOpen.bind(null, record),
+              },
+              auth: 'super', // 同时根据权限和业务控制是否显示
+              ifShow: () => {
+                return true;
+              },
+            },
+          ]"
+        />
+      </template>
+
       <template #id="{ record }"> 
         ID: {{ record.id }} 
       </template>
@@ -30,9 +101,10 @@
     </BasicTable>
   </div>
 </template>
+
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import { BasicTable, useTable, BasicColumn, TableImg } from '/@/components/Table';
+  import { BasicTable, useTable, BasicColumn, TableImg, TableAction } from '/@/components/Table';
   import { Tag, Avatar } from 'ant-design-vue';
   import { getAttachmentList } from '/@/api/sys/attachment';
   
@@ -92,26 +164,97 @@
       dataIndex: 'endTime',
     },
   ];
-
+  
   export default defineComponent({
     components: { 
       BasicTable, 
       TableImg, 
       Tag, 
-      Avatar 
+      Avatar,
+      TableAction,
     },
     setup() {
-      const [registerTable] = useTable({
-        title: '自定义列内容',
+      const [registerTable, { reload }] = useTable({
+        title: '附件管理',
         titleHelpMessage: '表格中所有头像、图片均为mock生成，仅用于演示图片占位',
         api: getAttachmentList,
-        columns: columns,
+        // 分页
+        pagination: { 
+          pageSize: 10, 
+          total: 100,
+          current: 1,
+          showQuickJumper: true,
+        },
+        loading: true,
         bordered: true,
         showTableSetting: true,
+        tableSetting: { 
+          fullScreen: true, 
+        },
+        useSearchForm: true,
+        rowKey: 'id',
+        
+        // 请求之前处理参数
+        beforeFetch: function() {
+
+        },
+        // 自定义处理接口返回参数
+        afterFetch: function(res) {
+          console.log(res);
+          return res;
+        },
+        // 查询条件请求之前处理
+        handleSearchInfoFn: function() {
+
+        },
+
+        // 额外的请求参数
+        searchInfo: {
+          "keywords": "123",
+        },
+        // 默认的排序参数
+        defSort: {},
+
+        columns: columns,
+        actionColumn: {
+          width: 250,
+          title: '操作',
+          dataIndex: 'action',
+          slots: { 
+            customRender: 'action',
+          },
+        },
       });
+
+      function handleReloadCurrent() {
+        reload();
+      }
+
+      function handleReload() {
+        reload({
+          page: 1,
+        });
+      }
+
+      function handleEdit(record: Recordable) {
+        console.log('点击了编辑', record);
+      }
+      function handleDelete(record: Recordable) {
+        console.log('点击了删除', record);
+      }
+      function handleOpen(record: Recordable) {
+        console.log('点击了启用', record);
+      }
 
       return {
         registerTable,
+
+        handleReloadCurrent,
+        handleReload,
+
+        handleEdit,
+        handleDelete,
+        handleOpen,
       };
     },
   });
